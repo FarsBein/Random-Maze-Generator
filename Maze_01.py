c0565014c0d50994f76e7f9a8ce0e7e1e2e3c1ba
Fars Bein <fars.bein@ryerson.ca>
2020-01-24 13:09:57 -0500

    clean up the big blocks in the maze

--- Maze_01.py
+++ Maze_01.py
@@ -78,23 +78,23 @@ def cleanUp():
             #             board[i][j+1] = ''
             #         else:
             #             board[i-1][j] = ''       
-            if j <= columns-4 and i <= rows-5:
-                print(f'[x: {j},y: {i}]')   
-                for r in range(4):
-                    for c in range(4):
-                        if r != 0 and 0 != c:
+            if j <= columns-3 and i <= rows-3: #rows-4
+                for r in range(3):
+                    for c in range(3):
+                        if r != 1 or 0 != 1:
                             checkedBox.append([i+r,j+c])
                         if board[i+r][j+c] == '.':
                             filledBox=False
-                if not filledBox:
-                    checkedBox=[]
                 if filledBox:
-                    for coordinates in checkedBox:
-                        finalCheckBox.append(coordinates)
-                    if [i,j] not in finalCheckBox:
-                        if i <= rows-1 and j <= columns :
+                    if [i+1,j+1] not in finalCheckBox:
+                        # print(f'[j: {j},i: {i}]','i < rows-1 : ',i < rows-1,'j < columns-1: ',j < columns-1) 
+                        if i <= rows-1 and j <= columns-1:
                             filledBoxesList.append([i,j])
+                            for coordinates in checkedBox:
+                                finalCheckBox.append(coordinates)
+                checkedBox=[]
                 filledBox=True
+    print(filledBoxesList)
     return filledBoxesList
 
             
@@ -117,6 +117,7 @@ def turn4(x,y,possibleTurns):
 
 
 def randomDot_6(Heads):
+    # for CrazyLoop in range(2):
     currentHead = []
     possibleTurns=[]
     tries=0
@@ -124,7 +125,8 @@ def randomDot_6(Heads):
     # while True:
     for x,y in Heads:
         # print(f'[x: {x},y: {y}]')   
-        board[y][x] = '.'
+        if y <= rows-1 and x <= columns-1:
+            board[y][x] = '.'
         if straight and x not in [1,columns-1,0] and y not in [1,rows-1,0]:
             # if [x,y] in [[round(columns/2),round(rows/2)],[2,2],[2,rows-3],[columns-3,rows-3],[columns-3,2]]:
             if board[y][x-1] == '.':
@@ -150,28 +152,31 @@ def randomDot_6(Heads):
         #         print('}')
         elif y >= 2 and x <= columns-2 and y<=rows-2 and x >=2:
             if rows-y > 2 and board[y+1][x] == '' and board[y+2][x] == '':
-                # if board[y+1][x+1] == '' and board[y+1][x-1] == '': #and board[y+2][x+1] == '' and board[y+2][x-1] == '':
-                possibleTurns.append(3)
+                if board[y+1][x+1] == '' and board[y+1][x-1] == '': #and board[y+2][x+1] == '' and board[y+2][x-1] == '':
+                    possibleTurns.append(3)
             if board[y-1][x] == '' and board[y-2][x] == '':
-                # if board[y-1][x+1] == '' and board[y-1][x-1] == '': #and board[y-2][x+1] == '' and board[y-2][x-1] == '':
-                possibleTurns.append(1)
+                if board[y-1][x+1] == '' and board[y-1][x-1] == '': #and board[y-2][x+1] == '' and board[y-2][x-1] == '':
+                    possibleTurns.append(1)
             if columns-x > 2 and board[y][x+1] == '' and board[y][x+2] == '':
-                # if board[y+1][x+1] == '' and board[y-1][x+1] == '': #and board[y+1][x+2] == '' and board[y-1][x+2] == '':
-                possibleTurns.append(0)
+                if board[y+1][x+1] == '' and board[y-1][x+1] == '': #and board[y+1][x+2] == '' and board[y-1][x+2] == '':
+                    possibleTurns.append(0)
             if board[y][x-1] == '' and board[y][x-2] == '':
-                # if board[y+1][x-1] == '' and board[y-1][x-1] == '': #and board[y+1][x-2] == '' and board[y-1][x-2] == '':
-                possibleTurns.append(2)
+                if board[y+1][x-1] == '' and board[y-1][x-1] == '': #and board[y+1][x-2] == '' and board[y-1][x-2] == '':
+                    possibleTurns.append(2)
             if len(possibleTurns) > 0:
                 currentHead = turn4(x,y,possibleTurns)
                 Heads.append(currentHead)
+                # print(currentHead)
             possibleTurns = []
             straight= True
-        # os.system('cls')
-        # printBoard()
-    # tries+=1
-    # Heads = Heads[::-1]
-    # if tries > 2:
-    #     break
+
+            # os.system('cls')
+            printBoard()
+            print()
+        # tries+=1
+        # Heads = Heads[::-1]
+        # if tries > 2:
+        #     break
 
 startFinish()
 # randomDot_2()
@@ -179,23 +184,30 @@ startFinish()
 # randomDot_4()
 # randomDot_5()
 
-print()
-Heads=[[round(columns/2),round(rows/2)],[2,2],[2,rows-3],[columns-3,rows-3],[columns-3,2],[2,round(rows/2)],[columns-3,round(rows/2)],[round(columns/2),rows-3],[round(columns/2),2]]
-for i in Heads:
-    for j in range(50):
-        randomDot_6([i])
-printBoard()
-
-print()
-secHeads=cleanUp()
+Heads = randomDot_6([[round(columns/2),round(rows/2)]])
 printBoard()
 
 print()
 
-for i in secHeads:
-    for j in range(30):
+
+
+# Heads=[[round(columns/2),round(rows/2)],[2,2],[round(columns/3),2],[2,rows-3],[columns-3,rows-3],[columns-3,2],[2,round(rows/2)],[columns-3,round(rows/2)],[round(columns/2),rows-3],[round(columns/2),2],[round(columns/3)+round(columns/3),2],[round(columns/3)+round(columns/3),round(rows/2)],[round(columns/3),round(rows/2)],[round(columns/3)+round(columns/3),rows-2],[round(columns/3),rows-2]]
+for j in range(60):
+    secHeads = cleanUp()
+    for i in secHeads:
         randomDot_6([i])
 
 printBoard()
+
+print()
+
+    
+
+
+printBoard()
+
+print()
+
+
 
 
